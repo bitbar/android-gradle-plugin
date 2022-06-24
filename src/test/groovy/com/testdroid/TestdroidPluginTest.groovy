36732d0eb4d2165fa90c9ebf4cc4dc3c4eecea88
michal.szpruta <michal.szpruta@bitbar.com>
2022-06-24 20:45:11 +0200

    Update versions

--- src/test/groovy/com/testdroid/TestdroidPluginTest.groovy
+++ src/test/groovy/com/testdroid/TestdroidPluginTest.groovy
@@ -3,11 +3,12 @@ package com.testdroid
 import com.android.build.gradle.AppPlugin
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class TestdroidPluginTest {
+
     @Test
-    public void testTestdroidPluginInit() {
+    void testTestdroidPluginInit() {
         Project project = ProjectBuilder.builder().build()
         project.plugins.apply(AppPlugin.class)
         project.apply plugin: 'testdroid'
